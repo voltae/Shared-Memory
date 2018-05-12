ddb7aeca2fcb11a38dc643096c9f1ec00654edb1
voltae <ic17b096@technikum-wien.at>
2018-05-12 11:39:42 +0200

    removed all ressource allocation and ressource freeing. now we can work on the ring buffer.

--- receiver_test.c
+++ receiver_test.c
@@ -2,87 +2,53 @@
 // Created by marcaurel on 11.05.18.
 //
 
-#include <stdio.h>
-#include <stdlib.h>  // for EXIT_SUCCESS etc
-
-#include <semaphore.h>  // for semaphores
-#include <fcntl.h>      // For O_* constants
-#include <sys/stat.h>   // For mode constants
-#include <sys/mman.h>   // for shared memory
-#include <unistd.h>     // for ftruncate() truncation shared memory
-
-#include <errno.h>      // for errno global constant
-#include <string.h>     // for strerror
-
-
-
+#include "common.h"
 #define ERROR (-1)
-#define MAPSIZE 20
+
 /* Global constant semaphore read */
 sem_t *readSemaphore;
-const char *semaphoreReadName = "/semaphore_read";
+sem_t *writeSemaphore;
 
 /* Global constant for shared memory */
-int fileDescr_sm;                                    // file descriptor for the shared memory (is stored on disk "/dev/shm")
-const char *sharedMemoryName = "/shared_memory";     // file name of the storing memory
-char  *sharedMemory;                           // the linked memory address in current address space
+                                // file descriptor for the shared memory (is stored on disk "/dev/shm")
+                          // the linked memory address in current address space
 
 /* Report Error and fre ressources */
-void BailOut(const char *message)
-{
-    if (message != NULL)
-    {
-        fprintf(stderr, "%s\n", message);
-    }
-}
 
 int main (int argc, char **argv)
 {
+    char  *sharedMemory;
+    size_t buffersize = 0; // buffersize of memory
+    int opt;    // option for getop
+    char **nonDigits = NULL;
+
     // check operants with getopt(3)
-    if (argc > 0)
-    {
-        printf("argv: %s", argv[1]);
+    while ((opt = getopt(argc,argv,"m:")) != ERROR)  {
+        long bufferTemp;
+        switch (opt)
+        {
+            case 'm':
+                bufferTemp = strtol(optarg,nonDigits, 10);
+                if ((bufferTemp == ERROR) || *nonDigits != NULL)
+                {
+                    /* error handing */
+                }
+                buffersize = (size_t)bufferTemp;
+                break;
+            default:
+                fprintf(stderr, "Usage: %s [-m] length\n", argv[0]);
+                exit(EXIT_FAILURE);
+        }
     }
 
     /* Initialize Semaphores TODO: change the initial number to a variable*/
-    readSemaphore = sem_open(semaphoreReadName, O_CREAT|O_EXCL, S_IRWXU, MAPSIZE);
-    if (readSemaphore == SEM_FAILED)
-    {
-        /* Error message */
-        fprintf(stderr, "Error in creating semaphore, %s\n", strerror(errno));
-        BailOut("Could not create Semaphore");
-    }
+    readSemaphore = createSemaphore(buffersize, 'r');
 
     fprintf(stdout, "Semaphore on adress: %p\n", semaphoreReadName);
 
 
     // initialize shared memory
-    fileDescr_sm = shm_open(sharedMemoryName, O_CREAT|O_EXCL|O_RDWR, S_IRWXU);
-    if (fileDescr_sm == ERROR)
-    {
-        fprintf(stderr, "Error in creating shared memory, %s\n", strerror(errno));
-        BailOut("Could not create shared memory");
-    }
-
-    // fixing the shared memory to a define size (coming from the agrv)
-    if (ftruncate(fileDescr_sm, MAPSIZE) == ERROR)
-    {
-        fprintf(stderr, "Error in truncating shared memory, %s\n", strerror(errno));
-        BailOut("Could not truncate shared memory");
-    }
-
-    int pagesize = sysconf(_SC_PAGE_SIZE);
-    printf("Pagesize: %d\n", pagesize);
-    /*blend the shared memory in current memory */
-    /* TODO: MAPSIZE should be size of page */
-    int pa_offset = MAPSIZE & (pagesize - 1);
-
-     sharedMemory =  mmap(NULL, MAPSIZE, PROT_READ|PROT_WRITE,MAP_SHARED, fileDescr_sm, 0);
-    if (sharedMemory == MAP_FAILED)
-    {
-        fprintf(stderr, "Error in mapping memory, %s\n", strerror(errno));
-        BailOut("Could not map the memory");
-    }
+    sharedMemory = createSharedMemory(buffersize);
 
         /* Semaphore wait process */
 
@@ -115,43 +81,10 @@ int main (int argc, char **argv)
 
 
     /* unmap the memory */
-    int unmapReturn = munmap(sharedMemory, MAPSIZE);
-    if (unmapReturn == ERROR)
-    {
-        fprintf(stderr, "Error in unmapping memory, %s\n", strerror(errno));
-        BailOut("Could not unmap memory\n");
-    }
-
-    /* close the memory file */
-    int closeMemory = close(fileDescr_sm);
-    if (closeMemory == ERROR)
-    {
-        fprintf(stderr, "Error in closing memory file, %s\n", strerror(errno));
-        BailOut("Could not close memory file\n");
-    }
-
-    /* unlink the memory file from /dev/shm/ */
-    if (shm_unlink(sharedMemoryName) == ERROR)
-    {
-        fprintf(stderr, "Error in unlinking memory file, %s\n", strerror(errno));
-        BailOut("Could not unlink memory file\n");
-    }
+    closeSharedMemory(sharedMemory, buffersize);
 
     /* Closes the semaphore */
-    int semaphore_close;
-    semaphore_close = sem_close(readSemaphore);
-    if (semaphore_close == -1)
-    {
-        fprintf(stderr, "Error in closing semaphore, %s\n", strerror(errno));
-        BailOut("Could not close Semaphore\n");
-    }
-
-     /* unlink the semaphore, if not done, error EEXIST */
-    if (sem_unlink(semaphoreReadName) == -1)
-    {
-        fprintf(stderr, "Error in unlinking semaphore, %s\n", strerror(errno));
-        BailOut("Could not unlink Semaphore\n");
-    }
+   closeSemaphores(readSemaphore, 'r');
 
     return EXIT_SUCCESS;
 }
