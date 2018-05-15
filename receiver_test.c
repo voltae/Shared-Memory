7cfb8d521faaa029b866762f11505c4724fee500
voltae <ic17b096@technikum-wien.at>
2018-05-15 10:02:09 +0200

    Simple version of the shared memory, synchronizing is done with the semaphores. It works well except the transport of EOF at the end. Sender must send EOF before terminating.

--- receiver_test.c
+++ receiver_test.c
@@ -6,16 +6,24 @@
 #define ERROR (-1)
 
 /* Global constant semaphore read */
-static sem_t *readSemaphore;
-//static sem_t *writeSemaphore;
+//char semaphoreReadName[NAMELLENGTH];  // Semaphore read Name with own annuminas uid
+//char semaphoreWriteName[NAMELLENGTH];  // Semaphore write Name with own annuminas uid
+//char sharedMemoryName[NAMELLENGTH];     // Shared memory name
+
+static sem_t *readSemaphore_local = NULL;
+static sem_t *writeSemaphore_local = NULL;
+static char *sharedMemory_local = NULL;
+
+unsigned int r;
+unsigned int w;
 
 static char *szCommand = "<not yet set>";
 
 /* Global constant for shared memory */
-                                // file descriptor for the shared memory (is stored on disk "/dev/shm")
-                          // the linked memory address in current address space
+// file descriptor for the shared memory (is stored on disk "/dev/shm")
+// the linked memory address in current address space
 
-/* Report Error and fre ressources */
+/* Report Error and free ressources */
 void print_usage(void)
 {
     fprintf(stderr, "Usage: %s [-m] length\n", szCommand);
@@ -26,7 +34,6 @@ int main (int argc, char **argv)
     /* store the progam name */
     szCommand = argv[0];
 
-    char  *sharedMemory;
     size_t buffersize = 0; // buffersize of memory
     int opt;    // option for getop
     //char **nonDigits = NULL;
@@ -41,9 +48,9 @@ int main (int argc, char **argv)
     while ((opt = getopt(argc,argv,"m:")) != -1)  {
         switch (opt)
         {
-           case 'm':
+            case 'm':
                 bufferTemp = strtol(optarg,NULL, 10);
-                if ((bufferTemp == ERROR) || bufferTemp > 1)
+                if ((bufferTemp == ERROR) || bufferTemp < 1)
                 {
                     /* error handing */
                     print_usage();
@@ -56,50 +63,115 @@ int main (int argc, char **argv)
         }
     }
 
+    if (strcmp(semaphoreReadName, "\0") == 0)
+    {
+        setRessourcesName();
+    }
+
     /* Initialize Semaphores TODO: change the initial number to a variable*/
-    readSemaphore = createSemaphore(buffersize, 'r');
+    if (readSemaphore_local == NULL)
+    { readSemaphore_local = sem_open(semaphoreReadName, 0); }
 
-    fprintf(stdout, "Semaphore on adress: %p\n", semaphoreReadName);
+    /* Initialize Semaphores TODO: change the initial number to a variable*/
+    if (writeSemaphore_local == NULL)
+    { writeSemaphore_local = sem_open(semaphoreWriteName, 0); }
 
+    // initialize the reader index
+    r = buffersize - 1;
+
+    // initialize the reading char for the shared memory
+    char readingChar;
 
     // initialize shared memory
-    sharedMemory = createSharedMemory(buffersize);
-
-        /* Semaphore wait process */
-
-    int semaphoreWait; 
-    semaphoreWait = sem_wait(readSemaphore);
-    if (semaphoreWait == ERROR)
+    if (sharedMemory_local == NULL)
     {
-        fprintf(stderr, "Error in waiting semaphore, %s\n", strerror(errno));
+        int shmfd = shm_open(sharedMemoryName, O_RDONLY, S_IRWXU);
+        if (shmfd == ERROR)
+        {
+            fprintf(stderr, "Error in creating shared memory, %s\n", strerror(errno));
+            BailOut("Could not create shared memory");
+        }
+
+        // fixing the shared memory to a define size (coming from the agrv)
+        if (ftruncate(shmfd, buffersize * sizeof(char)) == ERROR)
+        {
+            fprintf(stderr, "%s: Error in truncating shared memory: %lu, %s\n", szCommand, buffersize* sizeof(char), strerror(errno));
+            BailOut("Could not truncate shared memory");
+        }
+
+        /*blend the shared memory in current memory */
+        /* TODO: buffersize should be size of page */
+
+        /* blend the memory in READ ONLY */
+        sharedMemory_local = mmap(NULL, buffersize * sizeof(char), PROT_READ, MAP_SHARED, shmfd, 0);
+        if (sharedMemory_local == MAP_FAILED)
+        {
+            fprintf(stderr, "%s: Error in mapping memory, %s\n", szCommand, strerror(errno));
+            BailOut("Could not map the memory");
+        }
+
+    }
+
+    /* Semaphore wait process */
+
+    int retVal;
+    retVal = sem_wait(readSemaphore_local);
+    if (retVal == ERROR)
+    {
+        fprintf(stderr, "%s: Error in waiting semaphore, %s\n", szCommand, strerror(errno));
         BailOut("Could not wait for Semaphore");
     }
 
     printf("Entering in the critical region\n");
 
-    /* write to memory */
-    sharedMemory = "This is a test";
-
-
-    /* reading from memory */
-    fprintf(stdout, "Read from memory: %s\n", sharedMemory);
-    /* semaphore post process */
-    int semaphorePost;
-    semaphorePost = sem_post(readSemaphore);
-    if (semaphorePost == ERROR)
+    while (1)
     {
-        fprintf(stderr, "Error in post semaphore, %s\n", strerror(errno));
-        BailOut("Could not post for Semaphore\n");
-    }
+        // write index is the same as the read index. writer must wait
+        if (((w + 1) % buffersize) == r)
+        {
+            int semaphoreWait;
+            semaphoreWait = sem_wait(writeSemaphore_local);
+            if (semaphoreWait == ERROR)
+            {
+                fprintf(stderr, "Error in waiting semaphore, %s\n", strerror(errno));
+                BailOut("Could not wait for Semaphore");
+            }
+        }
 
-    printf("Exit the critical region\n");
+        if (sharedMemory_local[r] == EOF)
+        {
+            exit(1);
+        }
+
+        // print out the char to stdout
+        readingChar = sharedMemory_local[r];
+        fputc(readingChar, stdout);
+
+                // increment the counter
+        r = (r + 1) % buffersize;
+
+        int sem_write;
+        // if the writer process is asleep, wake it up
+        sem_getvalue(writeSemaphore_local,&sem_write);
+        if (sem_write == 0)
+        {
+            int retval = sem_post(writeSemaphore_local);
+            {
+                if (retval == ERROR)
+                {
+                    fprintf(stderr, "Error in posting semaphore, %s\n", strerror(errno));
+                    BailOut("Could not post for Semaphore");
+                }
+            }
+        }
+    }
 
 
     /* unmap the memory */
-    closeSharedMemory(sharedMemory, buffersize);
+    closeSharedMemory(sharedMemory_local);
 
     /* Closes the semaphore */
-   closeSemaphores(readSemaphore, 'r');
+    closeSemaphores();
 
     return EXIT_SUCCESS;
 }
