77f93432bc881564257533b4c5c532db0da200c4
ic17b022 <ic17b022@technikum-wien.at>
2018-05-20 13:47:03 +0200

    move option handling into funktion

--- sender.c
+++ sender.c
@@ -26,13 +26,17 @@ static char semaphoreReadName[NAMELLENGTH];  // Semaphore read Name with own ann
 static char semaphoreWriteName[NAMELLENGTH];  // Semaphore write Name with own annuminas uid
 static char sharedMemoryName[NAMELLENGTH];     // Shared memory name
 
+static size_t readParameters(const int argc, char* const argv[]);
+
 static void setRessourcesName(void);
 
-static void createSemaphores(size_t size,  const char* programName);
+static void createSemaphores(size_t size, const char* programName);
+
 static void createSharedMemory(size_t size, const char* programName);
 
-//static void removeRessources (void);
 static void bailOut(const char* porgramName, const char* message);
+
+void print_usage(const char* porgramName);
 
 static sem_t* readSemaphore = NULL;
 static sem_t* writeSemaphore = NULL;
@@ -47,27 +51,50 @@ static int fileDescr_sm;
 // file descriptor for the shared memory (is stored on disk "/dev/shm")
 // the linked memory address in current address space
 
-/* Report Error and free ressources */
-void print_usage(const char* porgramName) {
-    fprintf(stderr, "USAGE: %s [-m] length\n", porgramName);
-    exit(EXIT_FAILURE);
-}
+int main(int argc, char* argv[]) {
+    size_t buffersize = 0;
 
-/* Report Error and allocate ressources
- * Since we are in the sender process, we are responsable for allocating all ressources,
- */
-void bailOut(const char* porgramName, const char* message) {
-    if (message != NULL) {
-        fprintf(stderr, "%s: %s\n", porgramName, message);
+    buffersize = readParameters(argc, argv);
+
+    /* Set the ressource names */
+    setRessourcesName();
+    /* open the semaphores */
+    createSemaphores(buffersize, argv[0]);
+    /* Create shared Memory */
+    createSharedMemory(buffersize, argv[0]);
+    // initialize the reading char for the shared memory
+    short int readingChar;
+
+    while ((readingChar = fgetc(stdin)) != EOF) {
+        // write index is the same as the read index. writer must wait
+        short int semaphoreWait = sem_wait(writeSemaphore);
+        if (semaphoreWait == ERROR) {
+            fprintf(stderr, "Error in waiting semaphore, %s\n", strerror(errno));
+            bailOut(argv[0], "Could not wait for Semaphore");
+        }
+        /* read a char from shared memory */
+        sharedMemory[w] = readingChar;
+
+        int retval = sem_post(readSemaphore);
+        if (retval == ERROR) {
+            fprintf(stderr, "Error in posting semaphore, %s\n", strerror(errno));
+            bailOut(argv[0], "Could not post for Semaphore");
+        }
+        // increment the counter
+        w = (w + 1) % buffersize;
     }
 
+    /* Signalize the receiver that reading is not yet over EOF! has to be read */
+    sharedMemory[w] = EOF;
+    sem_post(readSemaphore);
+
+    return EXIT_SUCCESS;
 }
 
-int main(int argc, char** argv) {
+static size_t readParameters(const int argc, char* const argv[]) {
     int opt;    // option for getop
     int bOptionM = 0;   // Flag for the 'm' option
     int bError = 0;     // Flag for Option Error
-    size_t buffersize = 0;
     long bufferTemp = 0;
     char* stringInt = NULL;
 
@@ -107,46 +134,28 @@ int main(int argc, char** argv) {
     }
     if (argc != optind) {
         print_usage(argv[0]);
-    }
+    }   //TODO: revisit. hardcoding the 29 is probably not the way to go
     if ((errno == ERANGE) || (strcmp(stringInt, "\0") != 0) || (bufferTemp == 0) || (bufferTemp >> 29)) {
         print_usage(argv[0]);
     }
 
-    buffersize = (size_t) bufferTemp;
+    return (size_t) bufferTemp;
+}
 
-    /* Set the ressource names */
-    setRessourcesName();
-    /* open the semaphores */
-    createSemaphores(buffersize, argv[0]);
-    /* Create shared Memory */
-    createSharedMemory(buffersize, argv[0]);
-    // initialize the reading char for the shared memory
-    short int readingChar;
+/* Report Error and free ressources */
+void print_usage(const char* porgramName) {
+    fprintf(stderr, "USAGE: %s [-m] length\n", porgramName);
+    exit(EXIT_FAILURE);
+}
 
-    while ((readingChar = fgetc(stdin)) != EOF) {
-        // write index is the same as the read index. writer must wait
-        short int semaphoreWait = sem_wait(writeSemaphore);
-        if (semaphoreWait == ERROR) {
-            fprintf(stderr, "Error in waiting semaphore, %s\n", strerror(errno));
-            bailOut(argv[0], "Could not wait for Semaphore");
-        }
-        /* read a char from shared memory */
-        sharedMemory[w] = readingChar;
-
-        int retval = sem_post(readSemaphore);
-        if (retval == ERROR) {
-            fprintf(stderr, "Error in posting semaphore, %s\n", strerror(errno));
-            bailOut(argv[0], "Could not post for Semaphore");
-        }
-        // increment the counter
-        w = (w + 1) % buffersize;
+/* Report Error and allocate ressources
+ * Since we are in the sender process, we are responsable for allocating all ressources,
+ */
+void bailOut(const char* porgramName, const char* message) {
+    if (message != NULL) {
+        fprintf(stderr, "%s: %s\n", porgramName, message);
     }
 
-    /* Signalize the receiver that reading is not yet over EOF! has to be read */
-    sharedMemory[w] = EOF;
-    sem_post(readSemaphore);
-
-    return EXIT_SUCCESS;
 }
 
 static void setRessourcesName(void) {
@@ -168,7 +177,7 @@ static void createSemaphores(size_t size, const char* programName) {
     readSemaphore = sem_open(semaphoreReadName, O_CREAT | O_EXCL, S_IRWXU, 0);
     if (readSemaphore == SEM_FAILED) {
         /* Error message */
-        fprintf(stderr, "Error in creating read-semaphore, %s\n", strerror(errno));
+        fprintf(stderr, "Sender: Error in creating read-semaphore, %s\n", strerror(errno));
         bailOut(programName, "Could not create Semaphore");
     }
 
@@ -180,7 +189,7 @@ static void createSemaphores(size_t size, const char* programName) {
     writeSemaphore = sem_open(semaphoreWriteName, O_CREAT | O_EXCL, S_IRWXU, size);
     if (writeSemaphore == SEM_FAILED) {
         /* Error message */
-        fprintf(stderr, "Error in creating write-semaphore, %s\n", strerror(errno));
+        fprintf(stderr, "Sender: Error in creating write-semaphore, %s\n", strerror(errno));
         bailOut(programName, "Could not create Semaphore");
     }
 }
@@ -198,7 +207,7 @@ static void createSharedMemory(size_t size, const char* programName) {
     // initialize shared memory
     fileDescr_sm = shm_open(sharedMemoryName, O_CREAT | O_EXCL | O_RDWR, S_IRWXU);
     if (fileDescr_sm == ERROR) {
-        fprintf(stderr, "Error in opening shared memory, %s\n", strerror(errno));
+        fprintf(stderr, "Sender: Error in opening shared memory, %s\n", strerror(errno));
         bailOut(programName, "Could not create shared memory");
     }
 
