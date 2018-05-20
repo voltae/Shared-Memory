201b0eb0cf3b483e186553798d18929494d33dd2
ic17b022 <ic17b022@technikum-wien.at>
2018-05-20 14:44:23 +0200

    implement getSemaphores

--- common.c
+++ common.c
@@ -4,6 +4,39 @@
 
 #include "sharedMemory.h"
 
-char* getSemaphores(size_t size, const char* programName){
+static sem_t* readSemaphore;
+static sem_t* writeSemaphore;
 
+//TODO: rewrite into one errorhandling path
+semaphores getSemaphores(size_t size) {
+    semaphores sems;
+    if (readSemaphore != NULL)
+        sems.readSemaphore = readSemaphore;
+    else {
+        readSemaphore = sem_open(semaphoreReadName, O_CREAT | O_EXCL, S_IRWXU, 0);
+        if (readSemaphore == SEM_FAILED) {
+            /* Error message */
+            fprintf(stderr, "Sender: Error in creating read-semaphore, %s\n", strerror(errno));
+            sems.readSemaphore = NULL;
+            sems.writeSemaphore = NULL;
+            return NULL;
+        }
+    }
+
+    if (writeSemaphore != NULL)
+        sems.writeSemaphore = writeSemaphore;
+    else {
+        writeSemaphore = sem_open(semaphoreWriteName, O_CREAT | O_EXCL, S_IRWXU, size);
+        if (writeSemaphore == SEM_FAILED) {
+            /* Error message */
+            fprintf(stderr, "Sender: Error in creating write-semaphore, %s\n", strerror(errno));
+            sems.readSemaphore = NULL;
+            sems.writeSemaphore = NULL;
+            return NULL;
+        }
+    }
+
+    sems.readSemaphore = readSemaphore;
+    sems.writeSemaphore = writeSemaphore;
+    return sems;
 }
--- sharedMemory.h
+++ sharedMemory.h
@@ -8,23 +8,32 @@
 #include <stddef.h>
 #include <stdio.h>
 #include <stdlib.h>  // for EXIT_SUCCESS etc
-
 #include <semaphore.h>  // for semaphores
-
 #include <fcntl.h>      // For O_* constants
-
-
 #include <sys/stat.h>   // For mode constants
 #include <sys/mman.h>   // for shared memory
 #include <unistd.h>     // for ftruncate() truncation shared memory
-
-
-
 #include <errno.h>      // for errno global constant
 #include <string.h>     // for strerror
 #include <assert.h>
 
 
 #define NAMELLENGTH 14
-#define ERROR -1
+#define ERROR (-1)
+
+typedef struct semaphores{
+    sem_t* readSemaphore;
+    sem_t* writeSemaphore;
+} semaphores;
+
+typedef struct sharedmem {
+    int fileDescriptor;
+    short int* sharedMemory;
+} sharedmem;
+/* Global constant semaphore read */
+static char semaphoreReadName[NAMELLENGTH];  // Semaphore read Name with own annuminas uid
+static char semaphoreWriteName[NAMELLENGTH];  // Semaphore write Name with own annuminas uid
+static char sharedMemoryName[NAMELLENGTH];     // Shared memory name
+
+semaphores getSemaphores(size_t size);
 #endif //SHARED_MEMORY_SHAREDMEMORY_H
