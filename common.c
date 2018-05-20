16258c664f65e76ae7bb8729300dcaf983f51fd2
ic17b022 <ic17b022@technikum-wien.at>
2018-05-20 14:02:21 +0200

    create shared code

---
+++ common.c
@@ -0,0 +1,9 @@
+//
+// Created by manuel on 20/05/18.
+//
+
+#include "sharedMemory.h"
+
+char* getSemaphores(size_t size, const char* programName){
+
+}
---
+++ sharedMemory.h
@@ -0,0 +1,10 @@
+//
+// Created by manuel on 20/05/18.
+//
+
+#ifndef SHARED_MEMORY_SHAREDMEMORY_H
+#define SHARED_MEMORY_SHAREDMEMORY_H
+
+#include <stddef.h>
+
+#endif //SHARED_MEMORY_SHAREDMEMORY_H
