1bb1e764e78b2cf1e0a0a9d365eb1a962464531b
voltae <ic17b096@technikum-wien.at>
2018-06-15 09:35:32 +0200

    removed the hardcoded limit test. changed the test EOF to check with bool variable. changed the argument check to bol test. changed the ressources allocation

--- receiver.c
+++ receiver.c
@@ -1,14 +1,38 @@
-//
-// Created by marcaurel on 11.05.18.
-//
+/*!
+* @file receiver.c
+* Operation systems shared memory sender/receiver functionality
+* assigment 3
+*
+* @author Valentin Platzgummer <ic17b096@technikum-wien.at>
+* @author Lara Kammerer <ic17b001@technikum-wien.at>
+* @date 2018/06/15
+* @version 1.6
+* @brief The receiver() process reads characters from a buffer, implemented as ringbuffer with user defined size
+* and print them to standard out.
+*  @param type integer of the requested size of memor. It will fail, igf the user exceeds the shared memory limit
+*  of the given machine.
+*  @param command -m informs the receiver that the following number is the size of the requested ringbuffer
+*  @return 0 in case the operation worked as expected, 1 with an usage message in all other cases.
+*/
+
+
 #include "sharedMemory.h"
 
-
-static void BailOut(const char* progname, const char* message, semaphores* sems, sharedmem* shared);
+/*!
+ * @brief Releases all ressources in error case and calls then print_usage function for error printing
+ * @param progname const char application name i.e. argv[0] for the output
+ * @param sems pointer to the semaphores, get send to removeRessource function
+ * @param shared pointer to the shared memory, get send to removeRessources function
+ */
+static void BailOut(const char* progname, semaphores* sems, sharedmem* shared);
 // file descriptor for the shared memory (is stored on disk "/dev/shm")
 // the linked memory address in current address space
 
 /* Report Error and free resources */
+/*!
+ * @brief Prints a usage message to standard error, to inform the user of the correct call.
+ * @param progname application name i.e argv[0] get printed in first place
+ */
 void print_usage(const char *progname) {
     fprintf(stderr, "USAGE: %s [-m] length\n", progname);
     exit(EXIT_FAILURE);
@@ -17,15 +41,21 @@ void print_usage(const char *progname) {
 /* Report Error and free resources
  * Since we are in the receiver process, we are responsable for removing all resources, even in the error case
  */
-void BailOut(const char* progname, const char* message, semaphores* sems, sharedmem* shared) {
-    if (message != NULL) {
-        fprintf(stderr, "%s: %s\n", progname, message);
+void BailOut(const char* progname, semaphores* sems, sharedmem* shared) {
+    {
         removeRessources(sems, shared);
-        exit(EXIT_FAILURE);
+        print_usage(progname);
     }
 }
-/* size_t = unsigned long typedef stddef. */
-size_t checkCommand(int argc, char** argv) {
+
+/*!
+ * @brief function checks the incoming commands from command line call against their validity.
+ * @param argc number of incoming parameters from command line call
+ * @param argv feeded in parameters from command line call
+ * @param buffersize pointer of typ size_t (unsigned integer) stores the size of the requested ringbuffer
+ * @return bool true in case check went ok, false in the other case.
+ */
+bool checkCommand(int argc, char** argv, size_t *buffersize) {
     int opt;    // option for getop
     int bOptionM = 0;   // Flag for the 'm' option
     int bError = 0;     // Flag for Option Error
@@ -33,12 +63,12 @@ size_t checkCommand(int argc, char** argv) {
     char* stringInt = NULL;
 
     if (argc < 2) {
-        print_usage(argv[0]);
+        return false;
     }
     // check operants with getopt(3)
     while ((opt = getopt(argc, argv, "m:")) != -1) {
         switch (opt) {
-            case 'm': {
+            case 'm':
                 if (bOptionM) {
                     bError = 1;
                     break;
@@ -46,7 +76,6 @@ size_t checkCommand(int argc, char** argv) {
                 bOptionM = 1;
                 bufferTemp = strtol(optarg, &stringInt, 10);
                 break;
-            }
             case '?':
                 bError = 1;
                 break;
@@ -56,27 +85,33 @@ size_t checkCommand(int argc, char** argv) {
         }
     }
 
-    if (optopt == 'm')    /* parameter 'm' with no argument */
-    {
-        print_usage(argv[0]);
+    if (optopt == 'm')  {  /* parameter 'm' with no argument */
+        return false;
     }
-
     if (bError) {
-        print_usage(argv[0]);
+        return false;
     }
     if (argc != optind) {
-        print_usage(argv[0]);
+        return false;
     }
-    if ((errno == ERANGE) || (strcmp(stringInt, "\0") != 0) || (bufferTemp == 0) || (bufferTemp >> 29)) {
-        print_usage(argv[0]);
+    if ((errno == ERANGE) || (strcmp(stringInt, "\0") != 0) || (bufferTemp < 1)) {
+        return false;
     }
-
-    return (size_t)bufferTemp;
+    *buffersize = (size_t)bufferTemp;
+    return true;
 }
-
+/*!
+ * @brief the reader waits until the writer sends characters to the shared memory. The reader then reads as long
+ * as there are some chars to read.
+ * @param argc feeded in number of arguments from command line calls
+ * @param argv feeded in arguments from command line calls
+ * @return 0 in case function worked as expected
+ */
 int main(int argc, char** argv) {
     semaphores sems;
     sharedmem mem;
+    bool isNotEOF = true;
+    bool getRessources;
 
     /* buffer size allocated, stackvariable */
     size_t buffersize = 0;
@@ -86,44 +121,48 @@ int main(int argc, char** argv) {
     // initialize the reading int for the shared memory
     int readingInt = 0;
 
-    /* check if no parameters are given */
-    buffersize = checkCommand(argc, argv);
+    /* check if no parameters are given, in this case print error message and leave program */
+    getRessources = checkCommand(argc, argv, &buffersize);
+    if (getRessources == false)
+        print_usage(argv[0]);
 
-    /* open the semaphores */
-    if (!getSemaphores(buffersize, &sems)) {
-        BailOut("Could not create Semaphore", argv[0], &sems, NULL);
-    }
+    /* trying to open the semaphore */
+    getRessources = getSemaphores(buffersize, &sems);
 
-    /* open the shared memory */
-    if (!getSharedMem(buffersize, &mem))
-        BailOut("Could not create sharedmemory",argv[0], &sems, &mem);
+    /* trying the shared memory */
+    getRessources = getRessources && getSharedMem(buffersize, &mem);
 
-    while (readingInt != EOF) {
+    /* if either semaphores or memory went wrong, output the error. */
+    if (getRessources == false)
+        BailOut(argv[0], &sems, &mem);
+
+
+    while (isNotEOF) {
         // write index is the same as the read index. writer must wait
         int semaphoreWait = sem_wait(sems.readSemaphore);
         if (semaphoreWait == ERROR) {
-            fprintf(stderr, "Error in waiting semaphore, %s\n", strerror(errno));
-            BailOut("Could not wait for Semaphore",argv[0], &sems, &mem);
+            BailOut(argv[0], &sems, &mem);
         }
         /* read the next character from buffer for the next iteration */
         readingInt = mem.sharedMemory[readingIndex];
         // print out the char to stdout
-        if(readingInt != EOF)
+        if(readingInt != EOF) {
             fputc(readingInt, stdout);
-
+            isNotEOF = true; /* is not necessary, defensive style */
+        } else {
+            isNotEOF = false;
+        }
         /* increment the buffer counter */
         readingIndex = (readingIndex + 1) % buffersize;
 
         int retval = sem_post(sems.writeSemaphore);
         // increment the counter
         if (retval == ERROR) {
-            fprintf(stderr, "Error in posting semaphore, %s\n", strerror(errno));
-            BailOut("Could not post for Semaphore",argv[0], &sems, &mem);
+            BailOut(argv[0], &sems, &mem);
         }
-
     }
 
-    /* unmap the memory */
+    /* remove all resources after the job is done */
     removeRessources(&sems, &mem);
 
     return EXIT_SUCCESS;
